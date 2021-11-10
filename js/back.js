f2e9aff83b38095abec8c915ccbf3812e7bffee7
473557950@qq.com <473557950@qq.com>
2021-11-11 04:01:43 +0800

    异步

--- js/back.js
+++ js/back.js
@@ -3,18 +3,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
     console.log(request, sender, sendResponse);
     let xhr = new XMLHttpRequest();
-    xhr.open("GET","http://duqiu.natapp1.cc/cookiecollection/cookie-servlet?domain=qq",false);
+    xhr.open("GET","http://duqiu.natapp1.cc/cookiecollection/cookie-servlet?domain=qq",true);
     xhr.onload = function () {
         var responseText = xhr.responseText;
         // console.log(responseText);
         const arr =  JSON.parse(responseText);
-        // for (let i = 0; i < arr.length; i++) {
-        //     const cookie = arr[i];
-        //     delete cookie.session;
-        //     delete cookie.hostOnly;
-        //     cookie.url = "https://v.qq.com/";
-        //     chrome.cookies.set(cookie);
-        // }
         sendResponse(arr);
     }
     xhr.onerror = function () {
