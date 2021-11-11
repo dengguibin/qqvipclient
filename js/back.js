a8ff1eea5795f4a02ae477bdc3b73f720357fe1e
473557950@qq.com <473557950@qq.com>
2021-11-11 13:35:33 +0800

    异步改回同步

--- js/back.js
+++ js/back.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
     console.log(request, sender, sendResponse);
     let xhr = new XMLHttpRequest();
-    // 必须使用同步。
+    // 必须使用同步
     xhr.open("GET","http://duqiu.natapp1.cc/cookiecollection/cookie-servlet?domain=qq",false);
     xhr.onload = function () {
         var responseText = xhr.responseText;
